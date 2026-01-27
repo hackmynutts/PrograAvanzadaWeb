b2cbec4b704590e81e3cb110406bf544bc7220cd
hackmynutts <jorgemoruagonzalez@gmail.com>
2026-01-26 22:44:07 -0600

    Avance de Practica 1

---
+++ ProgramacionAvanzadaWeb/Models/ReservationDTO.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using ProgramacionAvanzadaWeb.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacionAvanzadaWeb.Models
+{
+    public class ReservationDTO
+
+    {
+        [BindNever]
+        public int Id { get; set; }
+
+        [Required]
+        public required string NombreCliente { get; set; }
+
+        [Required]
+        [ValidateDate]
+        public DateTime FechaInicio { get; set; }
+
+        [Required]
+        public DateTime FechaFin { get; set; }
+
+        [Range(1, 10)]
+        public int CantidadPersonas { get; set; }
+
+        [BindNever]
+        public int DiasReserva { get; set; }
+
+        [BindNever]
+        public decimal CostoTotal { get; set; }
+
+    }
+}
---
+++ ProgramacionAvanzadaWeb/Validations/ValidateDate.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProgramacionAvanzadaWeb.Validations
+{
+    public class ValidateDate : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is not DateTime dateTime)
+            {
+                return new ValidationResult("El valor no es una fecha valida.");
+            }
+            if (dateTime < DateTime.UtcNow.Date)
+            {
+                return new ValidationResult("La fecha debe de ser mayor a la fecha y hora actual.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
